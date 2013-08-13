33ffd3994254a1ae4dfb86e75109f8d8cd2e14c7
Justin Eldridge <eldridge@eldridge-pc.(none)>
2013-08-12 20:03:30 -0400

    Added contour test.

---
+++ code/denali/contour.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include "disjointsetforest.h"
+#include "scalargraph.h"
+#include "contourtree.h"
+
+using std::cout;
+using std::endl;
+
+int main() {
+    
+    // make the scalar graph on pg. 390 of Wenger
+    ScalarGraph g;
+    const int nodevalues[] = {25, 62, 45, 66, 16, 32, 64, 39, 58, 51, 53, 30};
+    for (int i=0; i<12; ++i) {
+        g.addNode(i+1, nodevalues[i]);
+    }
+
+    const int edges[][2] = {{1,2}, {2,3}, {4,5}, {5,6}, {7,8}, {8,9}, {10,11}, {11,12},
+                            {1,4}, {4,7}, {7,10}, {2,5}, {5,8}, {8,11}, {3,6}, {6,9}, {9,12},
+                            {1,5}, {2,6}, {4,8}, {5,9}, {7,11}, {8,12}};
+
+    for (int i=0; i<sizeof(edges)/sizeof(edges[0]); ++i) {
+        g.addEdge(edges[i][0], edges[i][1]);
+    }
+
+    ContourTree ct;
+    // compute the contour tree from the scalar graph (will sort inside function call)
+    ct.compute(g);
+
+}
